455b84041d19d2bc3858da289a1ced1db69d20bd
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-09-30 09:32:42 -0600

    Allow for importing stylesheets

--- page_contents/about/about.php
+++ page_contents/about/about.php
@@ -1,69 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>AKamienski</title>
-<?php include '/var/www/html/start.php'; ?>
-
-<style>
-<?php include '/var/www/html/pageStyle.css'; ?>
-
-#main {
-  width: 50%;
-}
-
-.btn {
-  background-color: DodgerBlue;
-  border: none;
-  color: white;
-  padding: 12px 30px;
-  cursor: pointer;
-  font-size: 20px;
-}
-
-/* Darker background on mouse-over */
-.btn:hover {
-  background-color: RoyalBlue;
-}
-
-</style>
-</head>
-<body>
-  <?php include '/var/www/html/header.php'; ?>
-
-  <center>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <div id="main">
-      <h1 class="w3-xlarge">ABOUT ME</h1>
-      <img src="/images/profile.jpg" width="30%" align="left" style="padding-right: 15px;"> My name is Arthur Kamienski, I am 21 years old and I am a student at the <a href="http://ufabc.edu.br">Federal University of ABC (UFABC)</a> in Santo André, São Paulo, Brazil. I am currently studying Computer Sciences and I should be done in December 2019.<br><br>
-
-      I consider myself a geek, with a deep love for programming, games and tecnology in general, and heavy influences of pop culture.<br><br>
-
-      I am a purple belt in Brazillian Jiu Jitsu, a sport which I have been practicing for nearly 5 years now.<br><br>
-
-      In my free time I like to read (I have just finished "A Song of Ice and Fire" and that is an accomplishment to me), to watch movies (I don't have any favourites, but I do like Interstellar, Guardians of the Galaxy and Amelie) and TV series (currently Brooklyn Nine-Nine and Game of Thrones) and most importantly: <b>I love to code!</b><br><br>
-
-      Throughout my still short carreer as a computer scientist, I have been in contact with many different programming languages and programming tools, most notably: Python, Java, C/C++,  SQL, JavaScript, PHP, HTML, CSS, Haskell, R, VHDL.<br><br>
-
-      Currently I am interested in the fields of Data Science, Machine Learning and Web development.<br><br>
-
-      If you want to, you can check out my resume! To download it, please click the button below (PDF file format).<br><br>
-
-      <form method="post" action="Resume-Arthur-Kamienski.pdf">
-      <button class="btn" type="submit"><i class="fa fa-download"></i> Download</button>
-    </form>
-
-      <hr>
-      <h1 class="w3-xlarge">ABOUT THE WEBSITE</h1>
-
-      This website was created in October 2018, as a way to share the applications that I was developing in Javascript with my friends and family.<br><br>
-
-      Soon, the whole website became a project itself, and I started improving it and adapting it to my likes and needs.<br><br>
-
-      Now the website is mostly used as a place where I can test new web applications and make school projects. It also serves as a portfolio and a place where I can show to any and everyone the things I have been working on. 
-    </div>
-  </center>
+<div>
+  <h1 class="w3-xlarge">ABOUT ME</h1>
+  <img src="/images/profile.jpg" width="30%" align="left" style="padding-right: 15px;"> My name is Arthur Kamienski, I am 21 years old and I am a student at the <a href="http://ufabc.edu.br">Federal University of ABC (UFABC)</a> in Santo André, São Paulo, Brazil. I am currently studying Computer Sciences and I should be done in December 2019.<br><br>
   
-  <?php include '/var/www/html/footer.php'; ?>
-</body>
-</html>
+  I consider myself a geek, with a deep love for programming, games and tecnology in general, and heavy influences of pop culture.<br><br>
+  
+  I am a purple belt in Brazillian Jiu Jitsu, a sport which I have been practicing for nearly 5 years now.<br><br>
+  
+  In my free time I like to read (I have just finished "A Song of Ice and Fire" and that is an accomplishment to me), to watch movies (I don't have any favourites, but I do like Interstellar, Guardians of the Galaxy and Amelie) and TV series (currently Brooklyn Nine-Nine and Game of Thrones) and most importantly: <b>I love to code!</b><br><br>
+  
+  Throughout my still short carreer as a computer scientist, I have been in contact with many different programming languages and programming tools, most notably: Python, Java, C/C++,  SQL, JavaScript, PHP, HTML, CSS, Haskell, R, VHDL.<br><br>
+  
+  Currently I am interested in the fields of Data Science, Machine Learning and Web development.<br><br>
+  
+  If you want to, you can check out my resume! To download it, please click the button below (PDF file format).<br><br>
+  
+  <form method="post" action="Resume-Arthur-Kamienski.pdf">
+    <button class="btn" type="submit"><i class="fa fa-download"></i> Download</button>
+  </form>
+  
+  <hr>
+  <h1 class="w3-xlarge">ABOUT THE WEBSITE</h1>
+  
+  This website was created in October 2018, as a way to share the applications that I was developing in Javascript with my friends and family.<br><br>
+  
+  Soon, the whole website became a project itself, and I started improving it and adapting it to my likes and needs.<br><br>
+  
+  Now the website is mostly used as a place where I can test new web applications and make school projects. It also serves as a portfolio and a place where I can show to any and everyone the things I have been working on. 
+</div>
