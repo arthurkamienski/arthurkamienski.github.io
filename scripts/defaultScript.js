1f43a2c5a98f38a1b4e69ccda80097602698eee2
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-10-03 20:14:30 -0600

    Revert "Revert to load"
    This reverts commit dcbc15c34896f2d79928dff3aefaf418ba98743c.

--- scripts/defaultScript.js
+++ scripts/defaultScript.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('head').load('/page_contents/page_head.html');
+  $('head').append('/page_contents/page_head.html');
   $("#header").load("/page_contents/header.html");
   $("#footer").load("/page_contents/footer.html");
 });
