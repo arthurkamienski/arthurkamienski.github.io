6f3e64e6a8ab9cdd7d5e04ef567f8f3eb29692a1
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-10-04 11:30:46 -0600

    Add tooltip effect to copy function

--- page_contents/contact/script.js
+++ page_contents/contact/script.js
@@ -3,4 +3,15 @@ document.title = 'Contact - ' + document.title;
 function copyEmail(d) {
     var emailInput = $(d).find("input")[0];
     navigator.clipboard.writeText(emailInput.value.replace('[at]', '@'));
+
+    var originalText = $(d).find("span.copy-text");
+    var copied = $(d).find("span.email-copied");
+
+    originalText.removeClass('fadein');
+    copied.addClass('fadeout');
+
+    setTimeout(function (){
+        originalText.addClass('fadein');
+        copied.removeClass('fadeout');
+    }, 3000);
 }
