b977ada2acedb21321c6a35dcfa8bd6a9e168e9d
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-10-03 23:56:36 -0600

    Add structure for email copy

--- page_contents/contact/script.js
+++ page_contents/contact/script.js
@@ -1,5 +1,8 @@
 document.title = 'Contact - ' + document.title;
 
-function copyText(e) {
-  alert(e); 
+function copyEmail(d) {
+  var email = $(d).text();
+  email.select();
+  email.setSelectionRange(0, 99999);
+
 }
