ddbedc889a71cff7468f517c2e10c30fd2b7290b
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-10-04 00:36:46 -0600

    Add copy email function

--- page_contents/contact/script.js
+++ page_contents/contact/script.js
@@ -2,7 +2,5 @@ document.title = 'Contact - ' + document.title;
 
 function copyEmail(d) {
     var emailInput = $(d).find("input")[0];
-    emailInput.select()
-    emailInput.setSelectionRange(0, 99999);
-    document.execCommand("copy");
+    navigator.clipboard.writeText(emailInput.value.replace('[at]', '@'));
 }
