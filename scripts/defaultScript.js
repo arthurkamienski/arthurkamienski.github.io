dcbc15c34896f2d79928dff3aefaf418ba98743c
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-10-03 20:00:57 -0600

    Revert to load

--- scripts/defaultScript.js
+++ scripts/defaultScript.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('head').append('/page_contents/page_head.html');
+  $('head').load('/page_contents/page_head.html');
   $("#header").load("/page_contents/header.html");
   $("#footer").load("/page_contents/footer.html");
 });
