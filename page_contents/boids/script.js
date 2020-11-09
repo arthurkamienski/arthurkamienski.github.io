e780d741a99ad6acd60c6e17ad34171f187a17ca
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-11-09 15:48:29 -0700

    Make birds into triangles

--- page_contents/boids/script.js
+++ page_contents/boids/script.js
@@ -5,16 +5,36 @@ var ctx = canvas.getContext("2d");
 var color = "#000000"
 
 bird = {
-  pos: [canvas.width/2, canvas.heigth/2]
+  x: canvas.width/2,
+  y: canvas.heigth/2,
+  speed: {x: 1, y: 0},
+  right: function() {
+    var dir = this.dir();
+    return {x: dir.y, y: -dir.x}
+  }
+
+  left: function() {
+    var dir = this.dir();
+    return {x: -dir.y, y: dir.x}
+  }
+  dir: function() {
+    return {x: this.x+this.speed.x, y: this.y+this.speed.y}
+  }
+
 }
 
 
 function drawBird(bird) {
-  const [x, y] = bird.pos;
+  var dir = bird.dir();
+  var left = bird.left();
+  var right = bird.right();
+
   ctx.fillStyle = color;
   ctx.beginPath();
-  ctx.arc(x, y, 5, 0, 2 * Math.PI);
-  ctx.fill;
+  ctx.moveTo(dir.x, dir.y);
+  ctx.lineTo(left.x, left.y);
+  ctx.lineTo(right.x, right.y);
+  ctx.fill();
 }
 
 
