468f6a86b167b1e268b77467ce1a1f1c72685a1b
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-11-12 14:22:17 -0700

    Remove scripts from html

--- page_contents/mario/script.js
+++ page_contents/mario/script.js
@@ -1,1 +1,15 @@
 document.title = 'Mario - ' + document.title;
+
+function addScript(src) {
+  var script = document.createElement('script');
+  script.src = src;
+
+  document.head.appendChild(script);
+}
+
+addScript("/page_contents/gamefiles/Character.js?v=1");
+addScript("/page_contents/gamefiles/Objects.js");
+addScript("/page_contents/gamefiles/Hitbox.js");
+addScript("/page_contents/gamefiles/Sound.js");
+addScript("/page_contents/gamefiles/Map.js");
+addScript("/page_contents/gamefiles/main.js");
