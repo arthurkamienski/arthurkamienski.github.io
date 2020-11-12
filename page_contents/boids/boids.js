28fdac803ec93690e185cd13b224ae90649ca7ed
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-11-12 12:22:55 -0700

    Change button margin, main padding

--- page_contents/boids/boids.js
+++ page_contents/boids/boids.js
@@ -64,12 +64,10 @@ function toggleSettings() {
     showSettings = false;
     $("#openSettings").css("background-color","");
     $(".settings-container").css("display", "none");
-    $(".settings-buttons").css("margin-bottom", "20px");
   } else {
     showSettings = true;
     $("#openSettings").css("background-color","#DDDDDD");
     $(".settings-container").css("display", "flex");
-    $(".settings-buttons").css("margin-bottom", "0px");
   }
 }
 
