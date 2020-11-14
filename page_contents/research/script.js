36a0b10efc4c0ed1f029bc8020238150ea8a398e
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-11-13 18:51:55 -0700

    Make buttons background dark when click

--- page_contents/research/script.js
+++ page_contents/research/script.js
@@ -3,16 +3,19 @@ document.title = 'Research - ' + document.title;
 function showAbstract(id) {
   var infoDir = $("#" + id + " .add-info"); 
   var abst    = $("#" + id + " .abstract"); 
-  var bibtex  = $("#" + id + " .bibtex"); 
+  var bibtex  = $("#" + id + " .bibtex");
+  var btn     = $("#" + id + " .abstract-btn");
   
   if (infoDir.css("display") == "none") {
     infoDir.css("display", "block");
     abst.css("display", "block");
     bibtex.css("display", "none");
+    btn.css("background-color", "lightgrey");
   } else {
     if(abst.css("display") == "none") {
       abst.css("display", "block");
       bibtex.css("display", "none");
+      btn.css("background-color", "lightgrey");
     } else {
       abst.css("display", "none");
     }
@@ -23,15 +26,18 @@ function showBib(id) {
   var infoDir = $("#" + id + " .add-info"); 
   var abst    = $("#" + id + " .abstract"); 
   var bibtex  = $("#" + id + " .bibtex"); 
+  var btn     = $("#" + id + " .bibtex-btn");
 
   if (infoDir.css("display") == "none") {
     infoDir.css("display", "block");
     abst.css("display", "none");
     bibtex.css("display", "block");
+    btn.css("background-color", "lightgrey");
   } else {
     if(bibtex.css("display") == "none") {
       abst.css("display", "none");
       bibtex.css("display", "block");
+      btn.css("background-color", "lightgrey");
     } else {
       bibtex.css("display", "none");
     }
