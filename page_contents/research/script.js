94b19e8d8d05fcaa75fb3917b9a1185f5dcb54aa
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-11-13 18:40:13 -0700

    Add showAbstract function

--- page_contents/research/script.js
+++ page_contents/research/script.js
@@ -1,1 +1,12 @@
 document.title = 'Research - ' + document.title;
+
+function showAbstract(id) {
+  $("#" + id + " .abstract").css("display", "block");
+  $("#" + id + " .add-info").css("display", "block");
+}
+
+function showBibtex(id) {
+}
+
+function showInfo(id) {
+}
