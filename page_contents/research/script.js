c76c5559d7669c6e7b1aac5a454fe170472db85e
Arthur Kamienski <arthur.kamienski@gmail.com>
2020-11-13 18:47:45 -0700

    Add button behaviour

--- page_contents/research/script.js
+++ page_contents/research/script.js
@@ -1,12 +1,43 @@
 document.title = 'Research - ' + document.title;
 
 function showAbstract(id) {
-  $("#" + id + " .abstract").css("display", "block");
-  $("#" + id + " .add-info").css("display", "block");
+  var infoDir = $("#" + id + " .add-info"); 
+  var abst    = $("#" + id + " .abstract"); 
+  var bibtex  = $("#" + id + " .bibtex"); 
+  
+  if (infoDir.css("display") == "none") {
+    infoDir.css("display", "block");
+    abst.css("display", "block");
+    bibtex.css("display", "none");
+  } else {
+    if(abst.css("display") == "none") {
+      abst.css("display", "block");
+      bibtex.css("display", "none");
+    } else {
+      abst.css("display", "none");
+    }
+  }
 }
 
 function showBibtex(id) {
+  var infoDir = $("#" + id + " .add-info"); 
+  var abst    = $("#" + id + " .abstract"); 
+  var bibtex  = $("#" + id + " .bibtex"); 
+
+  if (infoDir.css("display") == "none") {
+    infoDir.css("display", "block");
+    abst.css("display", "none");
+    bibtex.css("display", "block");
+  } else {
+    if(bibtex.css("display") == "none") {
+      abst.css("display", "none");
+      bibtex.css("display", "block");
+    } else {
+      bibtex.css("display", "none");
+    }
+  }
 }
 
 function showInfo(id) {
+  var infoDir = $("#" + id + " .add-info"); 
 }
