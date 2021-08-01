d9635216f036147537d233a2fd5195efa603a292
Arthur Kamienski <arthur.kamienski@gmail.com>
2021-08-01 10:21:09 -0600

    Fix contact email copy touch

--- page_contents/contact/script.js
+++ page_contents/contact/script.js
@@ -1,11 +1,31 @@
 document.title = 'Contact - ' + document.title;
 
-function copyEmail(d) {
-    var emailInput = $(d).find("input")[0];
+var onlongtouch;
+var timer = null;
+var touchduration = 800; //length of time we want the user to touch before we do something
+
+function touchstart(e) {
+    e.preventDefault();
+    if (!timer) {
+        timer = setTimeout(copyEmail, touchduration);
+    }
+}
+
+function touchend() {
+    //stops short touches from firing the event
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
+}
+
+async function copyEmail() {
+    timer = null;
+    var emailInput = $(this).find("input")[0];
     navigator.clipboard.writeText(emailInput.value.replace('[at]', '@'));
 
-    var originalText = $(d).find("span.copy-text");
-    var copied = $(d).find("span.email-copied");
+    var originalText = $(this).find("span.copy-text");
+    var copied = $(this).find("span.email-copied");
 
     originalText.removeClass('fadein');
     copied.addClass('fadeout');
@@ -15,3 +35,9 @@ function copyEmail(d) {
         copied.removeClass('fadeout');
     }, 3000);
 }
+
+$(document).ready(function() {
+  var divs = $('gmail, ualberta');
+
+  divs.on('click', copyEmail)
+});
