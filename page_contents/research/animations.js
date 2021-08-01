650845b9faa8dac1bdf7aba45c8560b516d401d4
Arthur Kamienski <arthur.kamienski@gmail.com>
2021-07-31 22:50:10 -0600

    Add animations

--- page_contents/research/animations.js
+++ page_contents/research/animations.js
@@ -1,80 +1,52 @@
+async function expandElement(e, t) {
+  e.css('transition', `max-height ${t}s ease-in-out`);
+  await new Promise(r => setTimeout(r, 10));
+  e.css('maxHeight', e.prop('scrollHeight') + 'px');
+  await new Promise(r => setTimeout(r, t*1000));
+  e.css({transition:''});
+  await new Promise(r => setTimeout(r, 10));
+  e.css('maxHeight', '100%');
+}
+
+async function collapseElement(e, t) {
+  e.css({transition:''});
+  await new Promise(r => setTimeout(r, 10));
+  e.css('maxHeight', e.prop('scrollHeight') + 'px');
+  await new Promise(r => setTimeout(r, 10));
+  e.css('transition', `max-height ${t}s ease-in-out`);
+  await new Promise(r => setTimeout(r, 10));
+  e.css('maxHeight', '0px');
+  await new Promise(r => setTimeout(r, t*1000));
+}
+
 async function toggleInfo(id, info) {
-  var infoDir = $("#" + id + " .add-info");
-  var content = $("#" + id + " .add-info ." + info);
+  var parent = $(`#${id}`);
+  var infoDir = parent.children('.add-info');
+  var content = infoDir.children(`.${info}`);
+  var btn = parent.children(`.${info}-btn`);
+
+  parent.children('.button').each(function() {
+    $(this).removeClass('selected');
+  })
+
+  btn.toggleClass('selected');
+  btn.css('pointer-events', 'none');
+
+  infoDir.children().each(function() {
+    if ($(this).css('maxHeight') != '0px') {
+      collapseElement($(this), 1);
+    }
+  })
 
   if (content.css('maxHeight') != '0px') {
-    content.css({transition:''});
-    await new Promise(r => setTimeout(r, 10));
-    content.css('maxHeight', content.prop('scrollHeight') + 'px');
-    await new Promise(r => setTimeout(r, 10));
-    content.css('transition', 'max-height 1s ease-in-out');
-    await new Promise(r => setTimeout(r, 10));
-    content.css('maxHeight', '0px');
+    collapseElement(content, 1);
     infoDir.toggleClass('visible');
-    await new Promise(r => setTimeout(r, 1000));
   } else {
     infoDir.toggleClass('visible');
-    content.css('transition', 'max-height 1s ease-in-out');
-    await new Promise(r => setTimeout(r, 10));
-    content.css('maxHeight', content.prop('scrollHeight') + 'px');
-    await new Promise(r => setTimeout(r, 1000));
-    content.css({transition:''});
-    content.css('maxHeight', '100%');
+    collapseElement(content, 1);
   }
-}
-
-function showAbstract(id) {
-  var infoDir = $("#" + id + " .add-info"); 
-  var abst    = $("#" + id + " .abstract"); 
-  var bibtex  = $("#" + id + " .bibtex");
-  var bibBtn  = $("#" + id + " .bib-btn");
-  var absBtn  = $("#" + id + " .abstract-btn");
   
-  if (infoDir.css("display") == "none") {
-    infoDir.css("display", "block");
-    abst.css("display", "block");
-    bibtex.css("display", "none");
-    absBtn.css("background-color", "lightgrey");
-    bibBtn.css("background-color", "");
-  } else {
-    if(abst.css("display") == "none") {
-      abst.css("display", "block");
-      bibtex.css("display", "none");
-      absBtn.css("background-color", "lightgrey");
-      bibBtn.css("background-color", "");
-    } else {
-      abst.css("display", "none");
-      infoDir.css("display", "none");
-      absBtn.css("background-color", "");
-    }
-  }
-}
-
-function showBib(id) {
-  var infoDir = $("#" + id + " .add-info"); 
-  var abst    = $("#" + id + " .abstract"); 
-  var bibtex  = $("#" + id + " .bibtex"); 
-  var bibBtn  = $("#" + id + " .bib-btn");
-  var absBtn  = $("#" + id + " .abstract-btn");
-
-  if (infoDir.css("display") == "none") {
-    infoDir.css("display", "block");
-    abst.css("display", "none");
-    bibtex.css("display", "block");
-    absBtn.css("background-color", "");
-    bibBtn.css("background-color", "lightgrey");
-  } else {
-    if(bibtex.css("display") == "none") {
-      abst.css("display", "none");
-      bibtex.css("display", "block");
-      absBtn.css("background-color", "");
-      bibBtn.css("background-color", "lightgrey");
-    } else {
-      bibtex.css("display", "none");
-      infoDir.css("display", "none");
-      bibBtn.css("background-color", "");
-    }
-  }
+  btn.css('pointer-events', 'auto');
 }
 
 function showInfo(id) {
@@ -91,29 +63,18 @@ function showInfo(id) {
 }
 
 async function expand(id) {
-  var div = $('#' + id);
-  var icon = $('#' + id + "-icon")
-  var btn = $('#' + id + "-expand-button");
+  var div = $(`#${id}`);
+  var icon = $(`#${id}-icon`);
+  var btn = $(`#${id}-expand-button`);
 
   btn.css('pointer-events', 'none');
 
   if (div.css('maxHeight') != '0px') {
-    div.css({transition:''});
-    await new Promise(r => setTimeout(r, 10));
-    div.css('maxHeight', div.prop('scrollHeight') + 'px');
-    await new Promise(r => setTimeout(r, 10));
-    div.css('transition', 'max-height 2s ease-in-out');
-    div.css('maxHeight', '0px');
-    await new Promise(r => setTimeout(r, 2000));
+    collapseElement(div, 2);
     icon.text('\u2795');
   } else {
     icon.text('\u2796');
-    div.css('transition', 'max-height 2s ease-in-out');
-    await new Promise(r => setTimeout(r, 10));
-    div.css('maxHeight', div.prop('scrollHeight') + 'px');
-    await new Promise(r => setTimeout(r, 2000));
-    div.css({transition:''});
-    div.css('maxHeight', '100%');
+    expandElement(div, 2);
   }
 
   btn.css('pointer-events', 'auto');
