7f766a759f56b383d2f578581f6631ef7a638eaf
Arthur Kamienski <arthur.kamienski@gmail.com>
2021-08-01 18:14:40 -0600

    Change topnav

--- scripts/defaultScript.js
+++ scripts/defaultScript.js
@@ -2,3 +2,11 @@ $(function(){
   $("#header").load("/page_contents/header.html");
   $("#footer").load("/page_contents/footer.html");
 });
+
+function goto(url) {
+    $('.topnav-btn.active').removeClass('active');
+    $('.topnav').toggleClass('expanded');
+    setTimeout(function() {
+        window.location.href = url;
+    }, 1000);
+}
