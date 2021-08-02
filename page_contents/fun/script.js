6156402bb8457d75da65d07bee729e85cf7bf642
Arthur Kamienski <arthur.kamienski@gmail.com>
2021-08-01 19:36:17 -0600

    Block buttons

--- page_contents/fun/script.js
+++ page_contents/fun/script.js
@@ -1,2 +1,7 @@
 document.title = 'Fun! - ' + document.title;
 $('#topnav-fun').toggleClass('active');
+
+function goToGame(url) {
+  $('.cell').css('pointer-events', 'none');
+  goTo(url);
+}
--- scripts/defaultScript.js
+++ scripts/defaultScript.js
@@ -3,10 +3,14 @@ $(function(){
   $("#footer").load("/page_contents/footer.html");
 });
 
-function goTo(url) {
+function goToTopnav(url) {
     $('.topnav-btn').css('pointer-events', 'none');
     $('.topnav-btn.active').removeClass('active');
     $('.topnav').toggleClass('expanded');
+    goTo(url);
+}
+
+function goTo(url) {
     $('.main').toggleClass('visible');
     setTimeout(function() {
         window.location.href = url;
